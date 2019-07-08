303e887398f0c7761e2a84fe267b5b71f0a9f4b6
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2019-07-08 09:52:48 -0600

    Add files via upload

---
+++ BSfilter.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-fq', type=str) # path/to/fastq/file
+parser.add_argument('-mC', type=float, default=1.0)    # methyl cytosine frequency allowed per read
+parser.add_argument('--count', type=int, default=1000)  # methyl cytosine count allowed per read
+
+args = parser.parse_args()
+
+start_time = datetime.datetime.now()
+print("started at: " + str(start_time))
+
+#gunzip files
+
+with open(args.fq, "rU") as handle:
+    """split fastq file name into its parts"""
+    fq_name, fq_ext = os.path.splitext(args.fq)
+
+    """print basic information about fastq file"""
+    read_count = len(list(SeqIO.parse(args.fq, "fastq")))
+    print("Found %i records in %s" % (read_count, args.fq))
+
+    for record in SeqIO.parse(handle, "fastq"):
+        """split fastq record descriptions into their parts"""
+        M01193_1, M01193_2, ninezeros, oneone, elevenohone,tenthousand, mate, N, zero, somesequence = record.description.split(':')
+        mate_num, mate_pair = mate.split(" ")
+        mate_id = mate_num + "/" + mate_pair
+        #print(mate_id)
+
+        """Make sequence uppercase"""
+        record.seq = record.seq.upper()
+        #fq_header = "@" + record.description
+
+        """Assign variables for filter"""
+        record.seq = record.seq.upper()
+        mC_count = record.seq.count("C")
+        N_count = len(record.seq)
+        mC_persistence = float(mC_count / N_count)
+        #print(fq_header)
+        #print(mate_id, record.seq, ":", mC_persistence, "-", mC_count,"/",N_count)
+        #print(record.description[])
+        #print(record.format("fastq"))
+
+        """define filter and print to file"""
+        new_fq_name = fq_name + "_filtered" + fq_ext
+        record_sequence = str(record.seq)
+
+        if mC_persistence <= args.mC and mC_count <= args.count:
+            with open(new_fq_name, 'a') as f_filter_out:
+                f_filter_out.write(record.format("fastq"))
+
+with open(new_fq_name, 'r') as filtered_fq:
+    read_count2 = len(list(SeqIO.parse(filtered_fq, "fastq")))
+    print("Found %i records in %s" % (read_count2, new_fq_name))
+
+
+#gzip files
+
+
+end_time = datetime.datetime.now()
+print("finished at: " + str(end_time))
